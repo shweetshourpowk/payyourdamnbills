bef0f0e63273b24645d33138bd0d6f47e46802fc
shweetshourpowk <rmccrory1993@gmail.com>
2015-02-18 19:32:06 -0500

    got pay your damn bills rolling. Adding a lot of what the app will look like. Mostly css and html changes. I figure since this is a series of apps they should all look "the same". so the menu is where we will get this consistency!

---
+++ script/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    init();
+});
+
+function init(){
+    setBindings();
+}
+
+function setBindings(){
+    var currentView = "firstView",
+        lastView = "";
+
+    $('nav div').click(function (e) {
+        var viewID = e.currentTarget.id + "View";
+        if(viewID != currentView){
+            lastView = currentView;
+            currentView = viewID;
+            if(viewID == "firstView"){
+                $("#" + currentView).transition({x:"0", zIndex:2}, 400);
+                $("#" + lastView).transition({zIndex:1});
+                $("#" + lastView).transition({x:"100%", delay: 400}, 400);
+            }else{
+                $("#" + currentView).transition({x:"-100%", zIndex:2}, 400);
+                $("#" + lastView).transition({zIndex:1});
+                $("#" + lastView).transition({x:"100%", delay: 400}, 400);
+            }
+
+        }
+    });
+}
+
